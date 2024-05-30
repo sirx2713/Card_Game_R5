fba321e468819c26b96242a74e10b03f8fcee6f3
Mabende <belindamabende@gmail.com>
2024-05-30 06:37:50 +0200

    Initial Commit

---
+++ Card_Game_R5/Card_Game_R5App.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Game_R5App.swift
+//  Card_Game_R5
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/05/30.
+//
+
+import SwiftUI
+
+@main
+struct Card_Game_R5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
